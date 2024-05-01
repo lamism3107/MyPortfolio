486c5f80c4e1222dd4439725ed8a0ee5de62fcc5
Lam Tran <lamtx.3107@gmail.com>
2024-05-02 00:31:47 +0700

    fix deploy

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,14 +1,9 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 // import prisma from "./connect";
 import { getServerSession } from "next-auth";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 export const authOptions = {
-  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_ID,
